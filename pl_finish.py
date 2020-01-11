38be48f6fba4159daa1e69c7c7d75617f11b78a9
Patrick <patrickcooper1995@gmail.com>
2020-01-11 15:11:04 -0500

    Update GitHub

--- pl_finish.py
+++ pl_finish.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import re
+import os
 
 # Read in results data
-pl_results = pd.read_csv(r'D:\Google Drive\PL Analysis\premier-league\results.csv')
+results_path = os.path.join('D:', 'Google Drive', 'PL Analysis', 'premier-league', 'results.csv')
+pl_results = pd.read_csv(results_path)
 
 # Filter the DF by boolean mask to fill a newly created 'winner' column with the home team name, away team name, or draw
 pl_home_wins = pl_results['result'] == 'H'
@@ -15,6 +14,10 @@ pl_results['winner'] = pl_results['winner'].fillna('Draw')
 
 # A dict container to hold as many tables as needed (final tables, weekly tables, etc.)
 pl_tables = {}
+
+# DataFrame used later to store our results - what was the table (and date) when the top team each season accumulated
+# enough points to mathematically avoid finishing last?
+final_gap_df = pd.DataFrame(columns=['Date', 'Top', 'Bottom', 'Top_Points', 'Bottom_Points', 'Matches_Played'])
 
 
 # Because naming conventions are not standard across multiple sources of data, standardize team names through mapping.
@@ -58,7 +61,9 @@ for key in schedule_keys:
 # Read in each csv with schedule data, dropping unnecessary columns, and mapping team names.
 # We'll also create a unique match_id by concatenating the strings in the Season, HomeTeam, and AwayTeam columns.
 for sc in schedule_keys:
-    t_df = pd.read_csv(r'D:\Google Drive\PL Analysis\english-premier-league\data\season-' + sc + '_csv.csv')
+    temp_path = os.path.join('D:', 'Google Drive', 'PL Analysis', 'english-premier-league', 'data',
+                             'season-' + sc + '_csv.csv')
+    t_df = pd.read_csv(temp_path)
     t_df = t_df.drop(t_df.columns[4:], axis=1)
     t_df = t_df.drop('Div', axis=1)
     t_df['Season'] = sc
@@ -129,37 +134,28 @@ def team_points(teams, temp_df, ppr, matchday):
 def get_table_on_date(df, t):
     ppr = 114
     matchday = 0
-    cant_lose_date = ""
-    cant_lose_team = ""
-    cant_win_team = ""
     top_to_bottom = 0
 
     for d in df['Date'].unique():
         date_df = df[df['Date'] <= d]
         table, matchday, ppr = team_points(t, date_df, ppr, matchday)
-        # print(ppr)
         top_to_bottom = table['Points'].max() - table['Points'].min()
         if top_to_bottom > ppr:
-            cant_lose_date = d
-            cant_lose_team = table.index[0]
-            cant_win_team = table.index[19]
-            return cant_lose_date, cant_lose_team, cant_win_team
+            return d, table, matchday
 
-
-final_gap_df = pd.DataFrame(columns=['Date', 'Top', 'Bottom'])
 
 # Create list of teams that were in the PL each season
 for s in schedule_keys:
     temp_df = pl_results[pl_results['season'] == s]
     teams = temp_df['home_team'].unique()
-    final_date, winner, loser = get_table_on_date(temp_df, teams)
-    data_dict = {'Date': final_date, 'Top': winner, 'Bottom': loser}
-    data_df = pd.DataFrame(data_dict, index=[0])
-    final_gap_df = pd.concat([final_gap_df, data_df])
-    # season_table = team_points(teams, temp_df)
-    # pl_tables.update({s: season_table})
-
-print(final_gap_df)
+    final_date, final_table, mp = get_table_on_date(temp_df, teams)
+    final_table['MP'] = mp
+    pl_tables.update({s: final_table})
+    answers_dict = {'Date': [final_date], 'Top': [final_table.index[0]], 'Bottom': [final_table.index[19]],
+                    'Top_Points': [final_table['Points'].max()], 'Bottom_Points': [final_table['Points'].min()],
+                    'Matches_Played': [final_table['MP'].max()]}
+    data_df = pd.DataFrame(answers_dict)
+    final_gap_df = pd.concat([final_gap_df, data_df], ignore_index=True)
 
 
 # This can be used for UI to run from the command line.
@@ -167,5 +163,26 @@ def ui_table():
     print('Please select from the following options: ' + '\n')
     for k in pl_tables.keys():
         print(str(k) + '\n')
-    selection = input(" ")
+    selection = input("")
     print(pl_tables[selection])
+
+
+def save_plot():
+    print('\nCreating and saving scatter plot...')
+    final_gap_df['Matches_Played'] = final_gap_df['Matches_Played'].astype(int)
+    final_gap_df['Top_Points'] = final_gap_df['Top_Points'].astype(int)
+    final_gap_df.drop(labels=['Date'], axis=1)
+    ax1 = final_gap_df.plot.scatter(x='Matches_Played', y='Top_Points').get_figure()
+    ax1.savefig('pl_scatter.pdf')
+
+
+def write_files():
+    print('\nWriting output files...')
+    with pd.ExcelWriter('tables.xlsx') as writer:
+        for k in pl_tables.keys():
+            pl_tables[k].to_excel(writer, sheet_name=k)
+
+    final_gap_df.to_excel('pl_analysis.xlsx')
+
+
+write_files()
